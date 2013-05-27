b2ff18bbfe762fc138c8dec7377fcb6ee4c28589
Kris St.Gabriel <kristianstgabriel@gmail.com>
2013-05-26 20:16:52 -0400

    The top menu is broken but getting there.

--- _site/assets/js/default.js
+++ _site/assets/js/default.js
@@ -40,26 +40,6 @@ $(document).ready(function(){
       }
   });
 
-  // 2.2 Menu for Mobile Devices
-  // If JS is enabled, attach the 'hide' class (only affects mobiles - special media query class)
-  $('.navigation').prepend('<a href="#" class="nav-toggle nodesktop notablet nomobile">Navigation <span class="arrow">+</span></a>');
-  $(".nav-toggle").removeClass('nomobile').addClass('showmobile');
-  $('.menu').addClass("nomobile");
 
-  // When the navigation button is clicked, toggle menu
-  $(".nav-toggle").click(function() {
-    if ( $('.menu:visible').length < 1 ) {
-      $('.menu').stop(true, true).slideDown(200, function() {
-        $('.nav-toggle .arrow').html('-');
-        $(this).removeClass('nomobile').removeAttr('style');
-      });
-    } else {
-      $('.menu').stop(true, true).slideUp(200, function() {
-        $('.nav-toggle .arrow').html('+');
-        $(this).addClass('nomobile').removeAttr('style');
-      });
-    };
-    return false;
-  });
 
 }); //end document ready();
